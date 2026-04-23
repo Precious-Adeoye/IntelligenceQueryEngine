5a6b35773c80135afc4162ba2e0a7d23615332ff
Precious-Adeoye <contact.preciousadoye@gmail.com>
2026-04-23 14:51:22 +0100

    add migration

--- IntelligenceQueryEngine/Migrations/20260423135016_init.Designer.cs
+++ IntelligenceQueryEngine/Migrations/20260423135016_init.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IntelligenceQueryEngine.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260423112333_change-table-name")]
-    partial class changetablename
+    [Migration("20260423135016_init")]
+    partial class init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
