b92a3493a1f9eb5a536883f3aba39aa3fb70d395
Precious-Adeoye <contact.preciousadoye@gmail.com>
2026-04-28 12:10:52 +0100

    feat: add identity

--- IntelligenceQueryEngine/Data/AppDbContext.cs
+++ IntelligenceQueryEngine/Data/AppDbContext.cs
@@ -1,16 +1,17 @@
 ﻿using IntelligenceQueryEngine.Model.Entity;
 using IntelligenceQueryEngine.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.Emit;
 using System.Text.Json;
 
 namespace IntelligenceQueryEngine.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<Profile> profiles { get; set; }
-    public DbSet<User> Users { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
     public DbSet<RequestLog> RequestLogs { get; set; }
 
@@ -22,13 +23,7 @@ public class AppDbContext : DbContext
             entity.HasIndex(e => e.name).IsUnique();
         });
 
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.HasIndex(e => e.GithubId).IsUnique();
-            entity.HasIndex(e => e.Email).IsUnique();
-            entity.ToTable("users");
-        });
+        
 
         // NEW - RefreshToken configuration
         modelBuilder.Entity<RefreshToken>(entity =>
