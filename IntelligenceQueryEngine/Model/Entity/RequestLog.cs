2ff05651b06efb7ba331ae3391fe5510fdffcd5c
Precious-Adeoye <contact.preciousadoye@gmail.com>
2026-04-28 11:03:16 +0100

    feat:add new request log entity

---
+++ IntelligenceQueryEngine/Model/Entity/RequestLog.cs
@@ -0,0 +1,14 @@
+﻿namespace IntelligenceQueryEngine.Model.Entity
+{
+    public class RequestLog
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string? UserId { get; set; }
+        public string Endpoint { get; set; } = string.Empty;
+        public string Method { get; set; } = string.Empty;
+        public int StatusCode { get; set; }
+        public string? IpAddress { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    }
+}
