dc56a7bb92e39e4b7cd9c540f4b2e615370fb989
Precious-Adeoye <contact.preciousadoye@gmail.com>
2026-04-28 12:12:16 +0100

    feat: add an identity class

---
+++ IntelligenceQueryEngine/Model/Entity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IntelligenceQueryEngine.Model.Entity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public long? GitHubId { get; set; }
+        public string? GitHubLogin { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+    }
+}
