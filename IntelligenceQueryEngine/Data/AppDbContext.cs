cb242ae8d5b5a30f56fba8c2a98e38c97f309f43
Precious-Adeoye <contact.preciousadoye@gmail.com>
2026-04-28 11:26:28 +0100

    feat:add new tables to the database

--- IntelligenceQueryEngine/Data/AppDbContext.cs
+++ IntelligenceQueryEngine/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using IntelligenceQueryEngine.Models;
+﻿using IntelligenceQueryEngine.Model.Entity;
+using IntelligenceQueryEngine.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.Emit;
 using System.Text.Json;
@@ -9,6 +10,9 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<Profile> profiles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<RequestLog> RequestLogs { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -16,6 +20,29 @@ public class AppDbContext : DbContext
         {
             entity.HasKey(e => e.id);
             entity.HasIndex(e => e.name).IsUnique();
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.GithubId).IsUnique();
+            entity.HasIndex(e => e.Email).IsUnique();
+            entity.ToTable("users");
+        });
+
+        // NEW - RefreshToken configuration
+        modelBuilder.Entity<RefreshToken>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.Token).IsUnique();
+            entity.ToTable("refresh_tokens");
+        });
+
+        // NEW - RequestLog configuration
+        modelBuilder.Entity<RequestLog>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.ToTable("request_logs");
         });
     }
 }
