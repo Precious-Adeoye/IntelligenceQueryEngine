5a0cb7dc267c18e3544c1f584e1aaa5a8c510cbe
Precious-Adeoye <contact.preciousadoye@gmail.com>
2026-04-28 11:26:03 +0100

    feat: add interfaces

---
+++ IntelligenceQueryEngine/Contract/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace IntelligenceQueryEngine.Contract
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(string userId, string role);
+        string GenerateRefreshToken();
+        ClaimsPrincipal? ValidateAccessToken(string token);
+        bool ValidateRefreshToken(string refreshToken, out string userId);
+    }
+}
